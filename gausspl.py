74c378fa0ffc3c9c3502a0760fc9db6eb1f824cf
gmolina8 <gmolina8@users.noreply.github.com>
2021-11-22 02:51:56 -0500

    ventana working

--- gausspl.py
+++ gausspl.py
@@ -2,13 +2,14 @@ import numpy as np
 from sustregr import sustregr
 
 def gausspl(A, b):
-    n = a.shape[0]
+    n = A.shape[0]
     M = np.insert(A, A.shape[0],b,axis=1).astype(float)
     
     for i in range(0,n-1):
         for j in range(i+1,n):
             if M[j,i] != 0:
                 M[j,i:n+1]=M[j,i:n+1]-(M[j,i]/M[i,i])*M[i,i:n+1]
+    print (sustregr(M))
     return sustregr(M) 
     
 # a = np.array([[2,3,1],
